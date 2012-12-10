9c32c4e31ec5249ff4479ad9c37e8e3b5dfda2cd
dotnil <jakeplus@gmail.com>
2012-12-10 10:45:15 +0800

    Inited the repo, yay

---
+++ README.md
@@ -0,0 +1,8 @@
+# Sans Google Wrap
+
+Google wraps the result on the search page.
+In China mainland, visiting that wrap link might time out because of youknowwho.
+
+But I do want use Google. So here is the chrome extension for removing that wrap.
+
+Sorry, Google.
---
+++ strip.js
@@ -0,0 +1,29 @@
+(function() {
+
+    if (!/^\w*.google.com(?:\.[a-z]+)?/.test(location.href)) {
+        return
+    }
+    var search = document.getElementById('search')
+
+    if (!search) {
+        return
+    }
+    var entries = search.getElementsByClassName('vsc')
+    var len = entries.length
+
+    for (var i = 0; i < len; i++) {
+        var e = entries[i]
+        var anchors = e.getElementsByTagName('a')
+
+        anchors = Array.prototype.slice.call(anchors, 0)
+        anchors.forEach(strip)
+    }
+
+    function strip(a, i) {
+        if (a.onmousedown) {
+            a.onmousedown = null
+            a.removeAttribute('onmousedown')
+            a.target = '_blank'
+        }
+    }
+})()
